b0493c8dbc95092c0d09b140304af7f6d7bc2049
Nikhila Bommareddy <nikhilaashokreddy@gmail.com>
2025-04-16 23:14:36 -0500

    updated print functionlity for defect 3434

--- calculator.py
+++ calculator.py
@@ -13,7 +13,7 @@ def divide(x, y):
     return x / y
 
 def calculator():
-    print("Simple Calculator")
+    print("Simple Calculator from branch")
     print("Select operation:")
     print("1. Add")
     print("2. Subtract")
